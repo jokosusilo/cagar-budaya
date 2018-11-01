74a40e523b85931bddcf2eef522eab5310291ac1
jokosusilo <jksusilo28@gmail.com>
2018-11-01 23:07:42 +0700

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:jokosusilo/cagar-budaya.git master:gh-pages
+git push -f https://github.com/jokosusilo/cagar-budaya.git master:gh-pages
 
 cd -
